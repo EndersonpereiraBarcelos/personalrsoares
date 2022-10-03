946158f138d4f76a0726910c432090a5c2af428f
endersonpb <souza98120@gmail.com>
2022-10-03 16:02:04 -0300

    recomenço

--- components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/NavBar.module.css";
-
-export default function NavBar() {
-  return (
-    <div className={styles.all}>
-      <h2>Foto com logo Personal</h2>
-    </div>
-  );
-}
