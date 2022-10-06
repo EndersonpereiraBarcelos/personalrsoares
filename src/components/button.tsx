78570e7fcadc8f7f3bb8a1d9a79700ae7e0d26da
endersonpb <souza98120@gmail.com>
2022-10-06 13:18:54 -0300

    update

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,14 +3,11 @@ import { useState } from "react";
 export default function Button() {
   const [count, setCount] = useState(0);
 
-  const incrementCount = () => {
-   
-  };
+  const incrementCount = () => {};
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={}>teste button</button>
     </div>
   );
 }
