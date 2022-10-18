55be917bb234ca726849980e598e1b1104e129ef
endersonpb <souza98120@gmail.com>
2022-10-18 16:06:45 -0300

    ajuste de texto com boostrap

--- components/fotoPrincipal.tsx
+++ components/fotoPrincipal.tsx
@@ -1,11 +1,37 @@
-import styled from "styled-components";
+import Carousel from "react-bootstrap/Carousel";
 import Image from "next/image";
-import Principal from "../public/personalrsoares/Patriocinio.png";
+import Foto1 from "../public/personalrsoares/Patriocinio.png";
 
-export default function FotoPrincipal() {
+export default function UncontrolledExample() {
   return (
-    <div>
-      <Image src={Principal} height={480} width={400} />
+    <div class="container">
+      <Carousel>
+        <Carousel.Item>
+          <Image src={Foto1} className="d-block" height={360} width={300} />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src={Foto1} className="d-block" height={360} width={300} />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src={Foto1} className="d-block" height={360} width={300} />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
