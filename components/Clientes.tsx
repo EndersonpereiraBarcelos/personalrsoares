b4e394df6bf8c4e567802b67ef31ecefebe61d77
endersonpb <souza98120@gmail.com>
2022-10-20 15:13:57 -0300

    antes e depois

--- components/Clientes.tsx
+++ components/Clientes.tsx
@@ -6,22 +6,24 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function RowColLayoutColWidthBreakpointExample() {
-  return (
-    <Container>
-      <Row md={4}>
-        <Col> <Image src={LorenaAD}/> lorena Bissoli </Col>
-        <Col xs={6}><Image src={LorenaAD}/></Col>
-        <Col><Image src={LorenaAD}/></Col>
-        <Col><Image src={LorenaAD}/></Col>
-      </Row>
-      <Row md={4}>
-        <Col> <Image src={LorenaAD}/> </Col>
-        <Col xs={6}><Image src={LorenaAD}/></Col>
-        <Col><Image src={LorenaAD}/></Col>
-        <Col><Image src={LorenaAD}/></Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+            <Row md={4}>
+                <Col> <Image src={LorenaAD} /> lorena Bissoli </Col>
+                <Col xs={6}><Image src={LorenaAD} /></Col>
+                <Col><Image src={LorenaAD} /></Col>
+            </Row>
+            <Row md={4}>
+                <Col> <Image src={LorenaAD} /> </Col>
+                <Col xs={6}><Image src={LorenaAD} /></Col>
+                <Col><Image src={LorenaAD} /></Col>
+
+            </Row>
+        </Container>
+    );
 }
 
 export default RowColLayoutColWidthBreakpointExample;
+
+
+
